ac5e6a38402449dd675bf2c7f7a425f18abea727
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-05 06:03:04 +0000

    add how_to_use, owner, route_api

--- app.py
+++ app.py
@@ -10,7 +10,21 @@ app = Flask(__name__)
 # Define the route to handle requests with a username parameter.
 @app.route('/')
 def home():
-  return 'hello'
+  return {
+    'how_to_use': [{
+      'text': 'you can use this API from link in route_api.'
+    }],
+    'owner': [{
+      'name': 'ridwaanhall',
+      'address': 'Sleman, DIY',
+      'my_love': 'Afida'
+    }],
+    'route_api': [{
+      'base_url': 'https://instagram-api.ridwaanhall.repl.co/',
+      'url': 'YOUR USERNAME',
+      'z_note': 'to use this api, you just add base_url + url'
+    }]
+  }
 
 @app.route('/<username>')
 def get_instagram_profile(username):
