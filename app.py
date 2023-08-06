4342ab1fa85459ef9c805ac1c7a9f1dc93b00d96
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-06 08:43:33 +0000

    Add route /profile, /story, and /media

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-from flask import Flask, request, jsonify
-import requests
-import json
-from Controller.IgController import process_instagram_profile
+from flask import Flask, jsonify
+from Controller.IgController import process_instagram_profile, process_instagram_story, process_instagram_media
 
 app = Flask(__name__)
 
-# Your function to make the API request and process the response
-
-# Define the route to handle requests with a username parameter.
 @app.route('/')
 def home():
   return {
@@ -21,15 +16,29 @@ def home():
     }],
     'route_api': [{
       'base_url': 'https://instagram-api.ridwaanhall.repl.co/',
-      'url': 'YOUR USERNAME',
-      'z_note': 'to use this api, you just add base_url + url'
+      'type_route': {
+        '/profile': 'USERNAME',
+        '/story': 'USERNAME',
+        '/media': 'to use this api, you just add base_url + url'
+        },
+      'z_note': None
     }]
   }
 
-@app.route('/<username>')
+@app.route('/profile/<username>')
 def get_instagram_profile(username):
-    result = process_instagram_profile(username)
+  result = process_instagram_profile(username)
+  return jsonify(result)
+
+@app.route('/story/<username>')
+def get_instagram_praofile(username):
+  result = process_instagram_story(username)
+  return jsonify(result)
+
+@app.route('/media', methods=['GET'])
+def get_instagram_media():
+    result = process_instagram_media()
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+  app.run(host='0.0.0.0',debug=True)
